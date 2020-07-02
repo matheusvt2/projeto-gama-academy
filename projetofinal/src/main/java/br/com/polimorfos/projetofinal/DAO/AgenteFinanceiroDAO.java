8e2d7dcced3363b2306dd1ceea3053bd2159d3e1
GABRIEL BONANI MACHADO <987303967@itaud.des.ihf>
2020-07-02 15:31:30 -0300

    template e js do login

--- projetofinal/src/main/java/br/com/polimorfos/projetofinal/DAO/AgenteFinanceiroDAO.java
+++ projetofinal/src/main/java/br/com/polimorfos/projetofinal/DAO/AgenteFinanceiroDAO.java
@@ -1,10 +1,14 @@
 package br.com.polimorfos.projetofinal.DAO;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import br.com.polimorfos.projetofinal.model.AgenteFinanceiro;
 
 public interface AgenteFinanceiroDAO  extends CrudRepository<AgenteFinanceiro, Integer>{
-
+	@Query("SELECT nome_agente,volume_transacional FROM MTB310_ag_financeiro")
+	public List<AgenteFinanceiro> findByCategory();
 }
 
