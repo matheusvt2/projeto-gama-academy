f097866db17a969624f6e9c1a340036119bdd22c
Marcel Eberlin <marcel_eberlin@hotmail.com>
2020-07-02 15:34:50 -0300

    Commit inicial

--- projetofinal/src/main/java/br/com/polimorfos/projetofinal/DAO/AgenteFinanceiroDAO.java
+++ projetofinal/src/main/java/br/com/polimorfos/projetofinal/DAO/AgenteFinanceiroDAO.java
@@ -5,6 +5,4 @@ import org.springframework.data.repository.CrudRepository;
 import br.com.polimorfos.projetofinal.model.AgenteFinanceiro;
 
 public interface AgenteFinanceiroDAO  extends CrudRepository<AgenteFinanceiro, Integer>{
-
 }
-
