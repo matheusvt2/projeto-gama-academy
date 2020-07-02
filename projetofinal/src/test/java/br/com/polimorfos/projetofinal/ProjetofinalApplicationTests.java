45e20f029c2c28f0f5faae10ccd326cdf264b241
Pedro Moraes <pgbmoraes@gmail.com>
2020-07-02 10:39:36 -0300

    Sprinboot + diagrama BD

---
+++ projetofinal/src/test/java/br/com/polimorfos/projetofinal/ProjetofinalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.polimorfos.projetofinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetofinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
